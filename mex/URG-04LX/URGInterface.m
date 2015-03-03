039c7a0d0d3fad4ff8343f0246b529fcbf435aea
Brian <brian.carvajal.meza@gmail.com>
2015-03-03 13:22:18 +0100

    .

--- mex/URG-04LX/URGInterface.m
+++ mex/URG-04LX/URGInterface.m
@@ -6,27 +6,27 @@ classdef URGInterface < handle
     methods
         %% Constructor - Create a new C++ class instance 
         function this = URGInterface()
-            this.objectHandle = class_interface_mex('new');
+            this.objectHandle = URGInterface_mex('new');
         end
         
         %% Destructor - Destroy the C++ class instance
         function delete(this)
-            class_interface_mex('delete', this.objectHandle);
+            URGInterface_mex('delete', this.objectHandle);
         end
         
         %% Connect - Connect the device at [port]
         function success = connect(this, port)
-            success = class_interface_mex('connect', this.objectHandle, port);
+            success = URGInterface_mex('connect', this.objectHandle, port);
         end
         
         %% Disconnect - Connect the device at [port]
         function success = disconnect(this)
-            success = class_interface_mex('disconnect', this.objectHandle);
+            success = URGInterface_mex('disconnect', this.objectHandle);
         end
 
         %% GetScan - Get [range] data from the lidar in mm
         function range = getScan(this, varargin)
-            range = class_interface_mex('getScan', this.objectHandle);
+            range = URGInterface_mex('getScan', this.objectHandle);
         end
 
 
